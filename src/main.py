240b6acce3e73c9eb3008d72a5dd840f08230edc
Mishieck <mishieckmwale@gmail.com>
2024-11-10 11:10:16 +0200

    refactor: use type hints

--- src/main.py
+++ src/main.py
@@ -2,121 +2,125 @@ import os
 import sys
 from groq import Groq
 from rich.console import Console
+from typing import List
+from dataclasses import dataclass
+from dataclasses import asdict
 
-MODEL = 'mixtral-8x7b-32768'
+MODEL = os.environ.get('MODEL')
 API_KEY = os.environ.get('GROQ_API_KEY')
 NERD_FONT_FLAG = '--unf'
 
-def run():
-  console.print(format_heading('assistant', model_name))
+@dataclass(frozen=True)
+class Message:
+  role: str # 'system' | 'assistant' | 'user'
+  content: str
+
+@dataclass(frozen=True)
+class TextStyles:
+  text: str
+  background: str
+
+  @property
+  def foreground_and_background(self) -> str:
+    return f'bold {self.text} on {self.background}'
+
+@dataclass(frozen = True)
+class Icons:
+  user: str
+  assistant: str
+
+@dataclass(frozen = True)
+class Header:
+  using_nerd_font: bool
+  role: str
+  text: str
+
+  def format(self) -> str:
+    if self.using_nerd_font: return self.format_using_nerd_font()
+    else: return  self.format_without_nerd_font()
+
+  def format_using_nerd_font(self) -> str:
+    icon = self.get_icon(nerd_font_icons)
+    heading = f'{icon}  {self.text}'
+    styles = text_styles[self.role]
+    open = Header.set_text_styles(styles.background, '')
+    close = Header.set_text_styles(styles.background, '')
+    middle = Header.set_text_styles(styles.foreground_and_background, heading)
+    return f'{open}{middle}{close}'
+
+  def format_without_nerd_font(self) -> str:
+    styles = text_styles[self.role].foreground_and_background
+    return Header.set_text_styles(styles, f' @{self.text} ')
+
+  def get_icon(self, icons: Icons) -> str:
+    return icons.user if self.role == 'user' else icons.assistant
+
+  def set_text_styles(styles, text) -> str:
+    return f'[{styles}]{text}[/]'
+
+client = Groq(api_key = API_KEY)
+system_message = Message(role = 'system', content = 'you are a helpful assistant.')
+messages: List[Message] = [asdict(system_message)]
+
+using_nerd_font = len(sys.argv) > 1 and sys.argv[1] == NERD_FONT_FLAG
+magenta = TextStyles(text = 'white', background = 'magenta')
+cyan = TextStyles(text = 'bold black on cyan', background = 'cyan')
+text_styles = { 'user': cyan, 'assistant': magenta}
+nerd_font_icons = Icons(user = '󰀄', assistant = '󰚩')
+
+username = os.getlogin()
+user_header = Header(using_nerd_font = using_nerd_font, role = 'user', text = username)
+model_name = MODEL.split('-')[0]
+ai_header = Header(using_nerd_font = using_nerd_font, role = 'assistant', text = model_name)
+
+headers = {
+  'user': user_header.format(),
+  'assistant': ai_header.format()
+}
+
+console = Console()
+
+def main():
+  print("Enter 'exit' to quit chat.\n")
+  console.print(headers['assistant'])
   print('How may I assist you?')
   print()
 
   while True:
     user_message = get_user_message()
-    if user_message['content'] == 'exit': break
-    messages.append(user_message)
+    if user_message.content == 'exit': break
+    messages.append(asdict(user_message))
     print()
     ai_message = get_ai_message()
-    messages.append(ai_message)
+    messages.append(asdict(ai_message))
     print('\n')
 
-def get_user_message():
-  console.print(format_heading('user', username))
-  content = input()
-  return create_user_message(content)
+def get_user_message() -> Message:
+  console.print(headers['user'])
+  return Message(role = 'user', content = input())
 
-def get_ai_message():
-  console.print(format_heading('assistant', model_name))
+def get_ai_message() -> Message:
+  console.print(headers['assistant'])
   stream = get_ai_message_stream(messages)
   content = ''
 
   for chunk in stream:
     new_content = chunk.choices[0].delta.content
-    print(new_content, end='')
+    print(new_content, end = '')
     if new_content: content = content + new_content
 
-  return create_ai_message(content)
+  return Message(role = 'assistant', content = content)
 
-def create_ai_message(content):
-  return create_message('assistant', content)
-
-def create_user_message(content):
-  return create_message('user', content)
-
-def create_message(role, content):
-  return { 'role': role, 'content': content }
-
-def get_ai_message_stream(messages):  
+def get_ai_message_stream(messages: List[Message]):  
   return client.chat.completions.create(
     messages = messages,
     model = MODEL,
-    temperature=0.5,
-    max_tokens=1024,
-    top_p=1,
-    stop=None,
-    stream=True,
+    temperature = 0.5,
+    max_tokens = 1024,
+    top_p = 1,
+    stop = None,
+    stream = True,
   )
 
-def get_nerd_font_flag(args: list[str]):
-  return len(args) > 1 and args[1] == NERD_FONT_FLAG
-
-def format_emoji_heading(role, name):
-  return underline(f'{emojis[role]} {name}')
-
-def format_nerd_font_heading(role, name):
-  styles = nerd_font_styles[role]
-  icon = nerd_font_icons[role]
-  heading = f'{icon}  {name}'
-  open = set_text_styles(styles['foreground'], '')
-  close = set_text_styles(styles['foreground'], '')
-  middle = set_text_styles(styles['text'], heading)
-  return f'{open}{middle}{close}'
-
-def underline(string):
-  return string + '\n' + '---'
-
-def set_text_styles(styles, text):
-  return f'[{styles}]{text}[/]'
-
-def get_model_name(id: str):
-  return id.split('-')[0]
-
-model = 'mixtral-8x7b-32768'
-username = os.getlogin()
-client = Groq(api_key = API_KEY)
-model_name = get_model_name(MODEL)
-system_message = create_message('system', 'you are a helpful assistant.')
-messages = [system_message]
-using_nerd_font = get_nerd_font_flag(sys.argv)
-format_heading = format_emoji_heading
-if using_nerd_font: format_heading = format_nerd_font_heading
-console = Console()
-
-emojis = {
-  'user': ':thinking_face:',
-  'assistant': ':robot_face:'
-}
-
-magenta = {
-  'text': 'bold white on magenta',
-  'foreground': 'magenta'
-}
-
-cyan = {
-  'text': 'bold black on cyan',
-  'foreground': 'cyan'
-}
-
-nerd_font_styles = {
-  'user': magenta,
-  'assistant': cyan
-}
-
-nerd_font_icons = {
-  'user': '󰀄',
-  'assistant': '󰚩'
-}
-
 if __name__ == '__main__':
-  run()
+  main()
